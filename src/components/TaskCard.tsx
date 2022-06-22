a821b112b341e2b34b587f892294f15a4c5bf7c4
JoeIt <josignaciot@gmail.com>
2022-06-22 16:24:06 -0300

    Project

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,24 @@
+import { Task } from '../interfaces/Task';
+
+interface Props {
+  task: Task;
+  deleteATask: (id: number) => void;
+}
+
+const TaskCard = ({ task, deleteATask }: Props) => {
+  return (
+    <div className="card card-body bg-secondary rounded-0 text-dark">
+      <h2>{task.title}</h2>
+      <p>{task.id}</p>
+      <p>{task.description}</p>
+      <button
+        className="btn btn-danger"
+        onClick={() => task.id && deleteATask(task.id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default TaskCard;
